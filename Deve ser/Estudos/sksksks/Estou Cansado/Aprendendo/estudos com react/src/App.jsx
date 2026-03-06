675db66da90507536e8bbf2d967a3c579bb548ee
CaioRO777 <mamilocremoso235@gmail.com>
2026-03-06 13:58:48 -0300

    Tentei Organizar melhor a Pasta, ficou bem melhor que antes.

--- Deve ser/Estudos/sksksks/Estou Cansado/Aprendendo/estudos com react/src/App.jsx
+++ Deve ser/Estudos/sksksks/Estou Cansado/Aprendendo/estudos com react/src/App.jsx
@@ -1,6 +1,7 @@
-
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
-import Adicao from './components/Adicao'
 import AppdeFlashcards from './components/AppdeFlashcards'
 
 function App() {
@@ -9,7 +10,6 @@ function App() {
   return (
     <>
       <h2>Praticando...</h2>
-      <Adicao />
       <AppdeFlashcards />
     </>
   )
